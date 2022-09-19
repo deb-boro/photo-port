92012d478459af724ff855c1a3fb96e3fb1eb4c9
deb boro <debajit.boro666@gmail.com>
2022-09-18 22:04:58 -0400

    updated index

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PhotoList from '../PhotoList'
 import { capitalizeFirstLetter } from '../../utils/helpers'
-
+/****************** */
 function Gallery(props) {
   const { currentCategory } = props
   return (
